b8882f2fbb9ec41dcff600ee88d8e2a2e1ace939
grigio <grigio.org@gmail.com>
2014-03-20 13:52:09 +0100

    opss, less data exposed = faster

--- server/publish.js
+++ server/publish.js
@@ -1,7 +1,7 @@
 Meteor.publish("allUsers", function (opts) {
   var opts = opts || {};
   var search = (opts.filterGeohash) ? {geohash:opts.filterGeohash} : {};
-  var res = Meteor.users.find( search ,{sort:{updatedAt:-1} } );
+  var res = Meteor.users.find( search ,{fields: {profile: 1, updatedAt:1}, sort:{updatedAt:-1} } );
 
   return res;
 });
