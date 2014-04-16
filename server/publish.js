1d834c4a81376a47374572ad0061763804054776
grigio <grigio.org@gmail.com>
2014-04-16 14:04:57 +0200

    less data published and loading position fix

--- server/publish.js
+++ server/publish.js
@@ -1,7 +1,13 @@
 Meteor.publish("allUsers", function (opts) {
   var opts = opts || {};
   var search = (opts.filterGeohash) ? {geohash:opts.filterGeohash} : {};
-  var res = Meteor.users.find( search ,{fields: {profile: 1, updatedAt:1}, sort:{updatedAt:-1} } );
+  var userFields = {
+    'profile.name':1, 'profile.geohash':1, 'profile.login':1, 'profile.location':1,
+    'profile.avatar_url':1, 'profile.blog':1, 'profile.html_url':1,
+    updatedAt:1
+  };
+  // userFields = {}
+  var res = Meteor.users.find( search ,{fields: userFields, sort:{updatedAt:-1} } );
 
   return res;
 });
